6bfd18bf21eb91c9f9e9423e9f21bd80290299f4
Milky-C <103466688+Milky-C@users.noreply.github.com>
2023-02-08 02:55:12 -0500

    basic enemy navmesh created
    I also just used an asset from the unity store I already had downloaded

--- Team_DDM/Assets/Scripts/enemyAI.cs
+++ Team_DDM/Assets/Scripts/enemyAI.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
 
     [SerializeField] Renderer enemyRenderer;
+    [SerializeField] NavMeshAgent navMesh;// allows for movement
+    [SerializeField] Transform headPos;// tracks head pos instead of from (0,0)
 
     [Header("-----Stats-----")]
     [SerializeField] int HP;
+
+    bool playerInRange;// bool if the player is within the range of detection of the enemy
+    Vector3 playerDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +25,42 @@ public class enemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (playerInRange)
+        //{
+        //    navMesh.SetDestination(gameManager.instance.player.transform.position);
+        //}
+        navMesh.SetDestination(gameManager.instance.player.transform.position);
+    }
+
+    /// <summary>
+    /// will eventually refactor into different functions
+    /// </summary>
+    void facePlayer()
+    {
+        playerDirection = gameManager.instance.player.transform.position - headPos.position;
+
+    }
+
+    /// <summary>
+    /// Player enters the enemies sphere collider
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    /// <summary>
+    /// player exits the enemies sphere collider
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
     }
 }
