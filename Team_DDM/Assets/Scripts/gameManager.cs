823c2dd16d9de1cbe52a24ddcba4b4d0ad5c2813
BeepySheepy <damiengelmann@gmail.com>
2023-02-08 19:01:50 -0500

    bullet and fixes

--- Team_DDM/Assets/Scripts/gameManager.cs
+++ Team_DDM/Assets/Scripts/gameManager.cs
@@ -9,7 +9,7 @@ public class gameManager : MonoBehaviour
 
     
     public GameObject player;// player object accesible thorugh other classes
-    // add playerScript
+    public playerController playerScript;
 
     // Start is called before the first frame update
     void Awake()
--- Team_DDM/Assets/Scripts/playerController.cs
+++ Team_DDM/Assets/Scripts/playerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [Header("----- Components -----")]
     [SerializeField] CharacterController controller;
 
+    [Header("----- Player Movement -----")]
     [Range(1, 10)] [SerializeField] int playerSpeed;
     [Range(0, 3)] [SerializeField] int jumpTimes;
     [Range(5, 50)] [SerializeField] int jumpSpeed;
     [Range(5, 150)] [SerializeField] int gravity;
+    [Range(1, 10)] [SerializeField] int HP;
+    [Header("----- Gun Attributes -----")]
     [Range(0.1f, 2.5f)] [SerializeField] float shootRate;
     [Range(1, 200)] [SerializeField] int shootDist;
     [Range(1, 10)] [SerializeField] int shootDamage;
+    [SerializeField] int bulletSpeed;
 
     int jumpsCurrent;
     bool isShooting;
@@ -64,10 +69,18 @@ public class playerController : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDist))
         {
-
+            if (hit.collider.GetComponent<IDamage>() != null)
+            {
+                hit.collider.GetComponent<IDamage>().takeDamage(shootDamage);
+            }
         }
 
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
     }
 }
