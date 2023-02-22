5451802ef48d9e07dbbc4ca325f3e852ac644377
BeepySheepy <damiengelmann@gmail.com>
2023-02-22 12:14:33 -0500

    prefab gun pickup

--- Team_DDM/Assets/Scripts/gunPickUp.cs
+++ Team_DDM/Assets/Scripts/gunPickUp.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class gunPickUp : MonoBehaviour
 {
     [SerializeField] gunStats gun;
+    [SerializeField] Transform shootPosition;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")){
+            gun.shootPos = shootPosition;
             gameManager.instance.playerScript.gunPick(gun);
             Destroy(gameObject);
         }
