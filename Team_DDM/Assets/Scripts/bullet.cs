3a66cdb93f108942f3c40e6c9b9b5b57f3ab8a79
Milky-C <103466688+Milky-C@users.noreply.github.com>
2023-03-01 16:00:19 -0500

    refactored bullet velocity + projectile shot enemy
    bullet velocity is now called within the bullet class itself. New projectile shot enemy: Bee Honey Lobber

--- Team_DDM/Assets/Scripts/bullet.cs
+++ Team_DDM/Assets/Scripts/bullet.cs
@@ -7,10 +7,19 @@ public class bullet : MonoBehaviour
     [SerializeField] int timer;
     public int bulletDamage;
 
+    Vector3 mShootDirection;
+    int mBulletSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, timer);
+        mShootDirection = Vector3.zero;
+    }
+
+    void Update()// will be use for homing bullet
+    {
+        
     }
 
     public void OnTriggerEnter(Collider other)
@@ -30,5 +39,31 @@ public class bullet : MonoBehaviour
 
         Destroy(gameObject);
 
+    }
+
+    /// <summary>
+    /// interface for the moved bullet whose velocity is defined within itself
+    /// </summary>
+    /// <param name="shootDirection">direction to multiply the vector by</param>
+    public void bulletShootInterface(Vector3 shootDirection, int bulletSpeed)
+    {
+        mShootDirection = shootDirection;// set mShootDirection
+        mBulletSpeed = bulletSpeed;
+        bulletShootVector();
+    }
+    /// <summary>
+    /// interface for the moved bullet whose velocity is defined within itself
+    /// </summary>
+    public void bulletShootInterface(int bulletSpeed)// overload
+    {
+        mBulletSpeed = bulletSpeed;
+        bulletShootVector();// the overload without a inputted vector send the user straight into the non-interface version
+    }
+    /// <summary>
+    /// the private function that controls the bullet movement
+    /// </summary>
+    void bulletShootVector()
+    {
+        GetComponent<Rigidbody>().velocity = mShootDirection * mBulletSpeed;
     }
 }
