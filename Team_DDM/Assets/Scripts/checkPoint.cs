7a7e56f4f1b038ffc458c32eaf2878b3bf381925
Brotato12 <104774258+Brotato12@users.noreply.github.com>
2023-03-02 15:59:27 -0500

    Check Points Works

--- Team_DDM/Assets/Scripts/checkPoint.cs
+++ Team_DDM/Assets/Scripts/checkPoint.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class checkPoint : MonoBehaviour
 {
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Checkpoint");
+            gameManager.instance.playerScript.checkpointHPTracker();
             gameManager.instance.playerSpawn.transform.position = transform.position;
             //Destroy(gameObject);
         }
     }
+
 }
