15cc9b0618c3b08a7c57c8a1e9be5bedc5459388
Milky-C <103466688+Milky-C@users.noreply.github.com>
2023-03-07 18:16:47 -0500

    Fully functioning switch

--- Team_DDM/Assets/Scripts/switchScript.cs
+++ Team_DDM/Assets/Scripts/switchScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class switchScript : MonoBehaviour, IDamage
 {
     [SerializeField] Renderer switchModel;
-    bool switchBool;
+    [SerializeField] GameObject door;
+    [SerializeField] bool doorSwitch;
+    [SerializeField] bool switchBool;// used for opening state
     Color modelColorOrig;
     private void Start()
     {
@@ -25,13 +27,28 @@ public class switchScript : MonoBehaviour, IDamage
         {
             switchModel.material.color = modelColorOrig;
         }
-    }
+        if (doorSwitch)
+        {
+            gameManager.instance.doorSwitch();
+        }else if(door != null)
+        {
+            door.SetActive(!switchBool);
+        }
 
+        // Optional can use GetSwitch
+
+    }
+    /// <summary>
+    /// toggles the switch
+    /// </summary>
     void ToggleSwitch()
     {
         switchBool = !switchBool;
     }
-
+    /// <summary>
+    /// Gets the bool of whether the switch is on or off
+    /// </summary>
+    /// <returns>switchBool</returns>
     public bool GetSwitch()
     {
         return switchBool;
