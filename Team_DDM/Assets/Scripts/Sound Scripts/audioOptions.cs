ee0699b2428d8739f98826e1da8b8e84872e3210
Brotato12 <104774258+Brotato12@users.noreply.github.com>
2023-03-14 20:35:57 -0400

    Gotta fix Audio Option

--- Team_DDM/Assets/Scripts/Sound Scripts/audioOptions.cs
+++ Team_DDM/Assets/Scripts/Sound Scripts/audioOptions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,11 +16,25 @@ public class audioOptions : MonoBehaviour
     public Slider seSlider;
     [SerializeField] TextMeshProUGUI musicVolumeText;
     [SerializeField] TextMeshProUGUI soundEffectVolumeText;
+    [SerializeField] GameObject audioSavedImage;
+    [SerializeField] GameObject audioClearedImage;
 
     void Start()
     {
-        revertMixer();
-        audioManager.instance.updateMixer();
+        
+        #region Problem B
+        //if (PlayerPrefs.HasKey("prefMusicVolume") || PlayerPrefs.HasKey("prefSoundEffectVolume"))
+        //{
+        //    Debug.Log("Player prefs saved1");
+        //    revertMixer();
+        //    audioManager.instance.updateMixer();
+        //}
+        //else
+        //{
+        //    Debug.Log("Player Pref saved2");
+        //    Default();
+        //}
+        #endregion
     }
     public void musicSlider(float _volume)
     {
@@ -32,14 +47,7 @@ public class audioOptions : MonoBehaviour
         soundEffectVolumeText.text = ((int)(_volume * 100)).ToString();
     }
 
-    public void saveMixer()
-    {
-        audioManager.instance.updateMixer();
-
-        PlayerPrefs.SetFloat("prefMusicVolume", musicVolume);
-        PlayerPrefs.SetFloat("prefSoundEffectVolume", soundEffectVolume);
-    }
-
+    #region Sound Test Functions
     public void musicTest()
     {
         StartCoroutine(testAudio1());
@@ -64,16 +72,77 @@ public class audioOptions : MonoBehaviour
         revertMixer();
         audioManager.instance.updateMixer();
     }
+    #endregion
 
     public void revertMixer()
     {
+        #region Problem A
+        //if (!PlayerPrefs.HasKey("prefMusicVolume") && !PlayerPrefs.HasKey("prefSoundEffectVolume"))
+        //{
+        //    musicVolume = 1;
+        //    soundEffectVolume = 1;
+        //}
+        //else
+        //{
+        //    musicVolume = PlayerPrefs.GetFloat("prefMusicVolume");
+        //    soundEffectVolume = PlayerPrefs.GetFloat("prefSoundEffectVolume");
+        //}
+        #endregion
+
         musicVolume = PlayerPrefs.GetFloat("prefMusicVolume");
         soundEffectVolume = PlayerPrefs.GetFloat("prefSoundEffectVolume");
+        mSlider.value = musicVolume;
+        seSlider.value = soundEffectVolume;
+
+        musicVolumeText.text = ((int)(musicVolume * 100)).ToString();
+        soundEffectVolumeText.text = ((int)(soundEffectVolume * 100)).ToString();
+    }
+    public void saveMixer()
+    {
+        audioManager.instance.updateMixer();
+
+        PlayerPrefs.SetFloat("prefMusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("prefSoundEffectVolume", soundEffectVolume);
+        mSlider.value = musicVolume;
+        seSlider.value = soundEffectVolume;
+
+        musicVolumeText.text = ((int)(musicVolume * 100)).ToString();
+        soundEffectVolumeText.text = ((int)(soundEffectVolume * 100)).ToString();
+
+        StartCoroutine(audioSaved());
+    }
+
+    IEnumerator audioSaved()
+    {
+        audioSavedImage.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        audioSavedImage.SetActive(false);
+    }
+
+    public void deleteData()
+    {
+        PlayerPrefs.DeleteAll();
+        Default();
+        StartCoroutine(audioCleared());
+    }
+    IEnumerator audioCleared()
+    {
+        audioClearedImage.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        audioClearedImage.SetActive(false);
+    }
+
+    void Default()
+    {
+        musicVolume = 1;
+        soundEffectVolume = 1;
 
         mSlider.value = musicVolume;
         seSlider.value = soundEffectVolume;
 
         musicVolumeText.text = ((int)(musicVolume * 100)).ToString();
         soundEffectVolumeText.text = ((int)(soundEffectVolume * 100)).ToString();
+
+        audioManager.instance.updateMixer();
     }
 }
